fcb574b1733415621563a842c1c9503eaa961e95
Bhokwe <165327116+Bhokwe@users.noreply.github.com>
2025-08-05 11:26:47 +0200

    Merging with fixed errors I hope

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,19 +8,21 @@ public class AudioManager : MonoBehaviour
         foreach(Sound s in sounds)
         {
             s.source= gameObject.AddComponent<AudioSource>();
+
             s.source.clip = s.clip;
             s.source.loop = s.loop;
         }
         PlaySound("Main Theme");
-        
+
     }
 
     public void PlaySound(string name)
     {
         foreach (Sound s in sounds)
         {
-           if(s.name == name)
-           s.source.Play();
+
+            if (s.name == name)
+                s.source.Play();
         }
 
 
--- Assets/Scripts/Sounds.cs
+++ Assets/Scripts/Sounds.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
-[System.Serializable]
-public class Sound
+
+//public class Sound
 {
     public string name;
     public AudioClip clip;
